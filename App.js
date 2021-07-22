5886715e3c7a29ec32d0cf489e14b544c8edbc0d
Alequin <j-cox03@hotmail.com>
2021-07-22 21:24:29 +0100

    Made a blank app

---
+++ App.js
@@ -0,0 +1,24 @@
+import { DarkTheme, NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { CurrentLuckPage } from "./src/current-luck-page";
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <StatusBar />
+      <NavigationContainer theme={DarkTheme}>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Root" component={CurrentLuckPage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+};
+
+export default App;
---
+++ src/current-luck-page.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Text } from "react-native";
+
+export const CurrentLuckPage = () => {
+  return <Text>Hey</Text>;
+};
